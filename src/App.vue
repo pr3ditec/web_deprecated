1c95fc1990ab6e6cb3f6dcb97a1ce79864cd5ad6
Lucas <123523359+lsbrel@users.noreply.github.com>
2023-11-07 15:12:56 -0300

    ✨ feat: cadastro de secretaria

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,16 @@ import authLayout from '@/layouts/auth-layout.vue';
 
 import { useAppStore } from '@/stores/index';
 import { useMeta } from '@/composables/use-meta';
+import { provide } from 'vue';
+
+import ApiConnection from './api/Api';
 
 const store = useAppStore();
+
+// Aqui vai ser feito um compartilhamento com todas as bibliotecas
+let connection = new ApiConnection('http://127.0.0.1:8000', 'ead8ae26e4375ea8280b336f4d438666')
+provide("api", connection)
+
 
 // meta
 useMeta({ title: 'Sales Admin' });
@@ -22,4 +30,8 @@ useMeta({ title: 'Sales Admin' });
 const mainLayout = computed(() => {
     return store.mainLayout === 'auth' ? authLayout : appLayout;
 });
+
+
+// para todo o sistma
+
 </script>
---
+++ src/helpers/MascaraInput.ts
@@ -0,0 +1,9 @@
+
+export default class MascarasInput {
+
+    public static cpf(cpf: string) : string {
+        
+        return cpf
+    }
+
+}
