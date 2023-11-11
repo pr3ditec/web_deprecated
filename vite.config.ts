3750b6e67eaee3fec24537edefc754652f111aab
Adriano K <adriano.kasp@gmail.com>
2023-11-11 19:21:16 -0300

    Update i18n and remove unused dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve, dirname } from 'node:path'
 import path from 'path';
-import vueI18n from '@intlify/vite-plugin-vue-i18n';
-// import vueI18n from '@intlify/unplugin-vue-i18n/messages'
+import { fileURLToPath } from 'node:url'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        vueI18n({
-            include: [path.resolve(__dirname, './src/locales/**')],
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
+  plugins: [
+    vue(),
+    VueI18nPlugin({
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
+    }),
+  ],
+  resolve: {
+    alias: {
+        '@': path.resolve(__dirname, './src'),
     },
-    optimizeDeps: {
-        include: ['quill'],
-    },
-});
+},
+optimizeDeps: {
+    include: ['quill'],
+},
+})
