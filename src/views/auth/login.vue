3b65c4ca52ad2f65544f2235ef3045b3352002de
Adriano K <adriano.kasp@gmail.com>
2023-11-11 01:58:46 -0300

    Fix login redirect message

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -297,7 +297,7 @@ const submitForm = async () => {
 };
 
 if (store.getUserToken()) {
-    Swal.fire("Você foi redirecionado pois já está logado."); // será removida, é pra vc saber o q está acontecendo
+    Swal.fire("Você foi redirecionado pois está logado."); // será removida, é pra vc saber o q está acontecendo
     router.push('/');
 }
 </script>
