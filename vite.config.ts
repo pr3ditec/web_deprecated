c84c8cc4a0692762c32cb913146754f74070f0bf
Adriano K <adriano.kasp@gmail.com>
2023-11-11 15:04:58 -0300

    Update i18n plugin and dev dependencies
    vite-plugin-vue-i18n -> unplugin-vue-i18n

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import vueI18n from '@intlify/vite-plugin-vue-i18n';
+// import vueI18n from '@intlify/vite-plugin-vue-i18n';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/messages'
 
 export default defineConfig({
     plugins: [
         vue(),
-        vueI18n({
-            include: path.resolve(__dirname, './src/locales/**'),
+        VueI18nPlugin({
+            include: [path.resolve(__dirname, './src/locales/**')],
         }),
     ],
     resolve: {
