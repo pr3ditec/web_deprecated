45dceb6bcb4abc26479c439a82113c3e9664f597
Adriano K <adriano.kasp@gmail.com>
2023-11-12 15:39:24 -0300

    Add format command, Update ESLint configuration, Docker Compose file, package.json scripts, Prettier configuration, Vite configuration, and index.html

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:vue/recommended'],
+    plugins: ['prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+    },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve, dirname } from 'node:path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve, dirname } from 'node:path';
 import path from 'path';
-import { fileURLToPath } from 'node:url'
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { fileURLToPath } from 'node:url';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueI18nPlugin({
-      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
-    }),
-  ],
-  resolve: {
-    alias: {
-        '@': path.resolve(__dirname, './src'),
+    plugins: [
+        vue(),
+        VueI18nPlugin({
+            include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-},
-optimizeDeps: {
-    include: ['quill'],
-},
-})
+    optimizeDeps: {
+        include: ['quill'],
+    },
+});
