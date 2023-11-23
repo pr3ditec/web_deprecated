eddad9ce77f5aace2e3a82393ef5078e55b0dd80
Adriano K <adriano.kasp@gmail.com>
2023-11-23 00:53:43 -0300

    Add store import and initialization

--- src/components/layout/LogoFull.vue
+++ src/components/layout/LogoFull.vue
@@ -1,3 +1,9 @@
+<script lang="ts" setup>
+import { useAppStore } from "@/stores/index";
+
+const store = useAppStore();
+</script>
+
 <template>
     <div class="w-48 block lg:w-72 ms-10">
         <img
