b1abb47526fba94899f5a56b02784ec5ba179ea7
Adriano K <adriano.kasp@gmail.com>
2023-11-28 00:38:52 -0300

    Update ESLint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    parser: 'vue-eslint-parser',
+    extends: ["eslint:recommended", "plugin:vue/recommended"],
+    plugins: ['vue', "prettier"],
+    rules: {
+        'vue/component-tags-order': ['error', {
+            'order': ['script', 'template', 'style']
+        }],
+        "prettier/prettier": "error",
+    },
+};
--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    extends: ["eslint:recommended", "plugin:vue/recommended"],
-    plugins: ["prettier"],
-    rules: {
-        "prettier/prettier": "error",
-    },
-};
