5bc210acb8cf45f66b0de311f06353c0994ff5bf
Lucas <123523359+lsbrel@users.noreply.github.com>
2023-11-29 11:43:58 -0300

    ✨ feat: atendimento e pre agendamento

--- src/views/agenda/modal-pre-agendamento.vue
+++ src/views/agenda/modal-pre-agendamento.vue
@@ -7,62 +7,133 @@ export default {
     components: {
         Multiselect,
     },
+    props: {
+        dataAgendar: Object,
+        medico: Number,
+    },
     data() {
         return {
             request: new ApiConnection(),
             selectInput: "",
-            selectOptions: [],
+            preAgendamentoShowSelect: true,
+            preAgendamento: [],
+            retornoAgenda: [],
             radioTipo: "consulta",
         };
     },
     methods: {
-        setIsOpen(value) {
-            this.isOpen = value;
+        /** EMITS */
+        emitPropostaAgendamento() {
+            this.$emit("update:modalAgendamento", {
+                tipo: this.radioTipo,
+                modalDados: this.selectInput,
+            });
+        },
+        emitFecharModal() {
+            this.$emit("update:fecharModal");
+        },
+        /** EMITS */
+
+        async buscarDadosDePreAgendamento() {
+            await this.request
+                .pegarDadosApi(`/pre-agendamento/medico/${this.medico}`)
+                .then((response) => {
+                    this.preAgendamento = response.list;
+                });
+        },
+        async buscarDadosParaRetorno() {
+            await this.request
+                .pegarDadosApi(`/agendamento/medico/${this.medico}`)
+                .then((response) => {
+                    this.retornoAgenda = response.list;
+                });
+        },
+        limparVariaveis() {
+            this.selectInput = "";
         },
     },
-    async created() {},
+    watch: {
+        radioTipo() {
+            if (this.radioTipo == "consulta") {
+                this.limparVariaveis();
+                this.preAgendamentoShowSelect = true;
+            } else {
+                this.limparVariaveis();
+                this.preAgendamentoShowSelect = false;
+            }
+        },
+    },
+    async created() {
+        this.limparVariaveis();
+        this.buscarDadosDePreAgendamento();
+        this.buscarDadosParaRetorno();
+    },
 };
 </script>
 <template>
-    <Transition>
+    <Transition :duration="200">
         <div
-            class="flex flex-col items-center align-items-start z-100 bg-white shadow-md w-1/2 mx-auto p-5 rounded-lg">
-            <h1 class="text-lg mt-4">Selecione o paciente</h1>
-            <hr class="w-75 m-2" />
-            <div class="flex flex-row items-center gap-3 mt-2 mb-4">
-                <label class="inline-flex">
-                    <input
-                        type="radio"
-                        name="default_text_color"
-                        class="form-radio"
-                        value="consulta"
-                        v-model="radioTipo" />
-                    <span>{{ $t("appointment") }}</span>
-                </label>
-                <label class="inline-flex">
-                    <input
-                        type="radio"
-                        name="default_text_color"
-                        class="form-radio peer-checked"
-                        value="retorno"
-                        v-model="radioTipo" />
-                    <span>{{ $t("return") }}</span>
-                </label>
+            class="absolute inset-0 flex items-center justify-center w-full h-full top-0 left-0 bg-dark bg-opacity-50"
+            style="z-index: 2000 !important">
+            <div
+                class="flex flex-col items-center bg-white shadow-lg w-1/3 mx-auto p-5 rounded-lg">
+                <div class="flex flex-row w-full justify-end">
+                    <span
+                        class="text-2xl font-bold text-dark opacity-30 uppercase cursor-pointer"
+                        @click="emitFecharModal"
+                        >x</span
+                    >
+                </div>
+                <h1 class="text-xl font-semibold mt-4">
+                    {{ $t("select_patient") }}
+                </h1>
+                <hr class="w-75 m-2" />
+                <div class="flex flex-row justify-around gap-3 mt-2 mb-2 w-1/2">
+                    <label class="inline-flex">
+                        <input
+                            type="radio"
+                            name="default_text_color"
+                            class="form-radio"
+                            value="consulta"
+                            v-model="radioTipo" />
+                        <span class="uppercase">{{ $t("appointment") }}</span>
+                    </label>
+                    <label class="inline-flex">
+                        <input
+                            type="radio"
+                            name="default_text_color"
+                            class="form-radio peer-checked"
+                            value="retorno"
+                            v-model="radioTipo" />
+                        <span class="uppercase">{{ $t("return") }}</span>
+                    </label>
+                </div>
+                <div class="w-full p-4">
+                    <multiselect
+                        v-show="preAgendamentoShowSelect"
+                        v-model="selectInput"
+                        :options="preAgendamento"
+                        track-by="id"
+                        label="paciente_nome"
+                        class="custom-multiselect"
+                        :searchable="true"
+                        placeholder="Selecione um paciente"></multiselect>
+                    <multiselect
+                        v-show="!preAgendamentoShowSelect"
+                        v-model="selectInput"
+                        :options="retornoAgenda"
+                        track-by="agenda_id"
+                        label="paciente"
+                        class="custom-multiselect"
+                        :searchable="true"
+                        placeholder="Selecione um paciente"></multiselect>
+                </div>
+                <button
+                    class="btn btn-sm btn-primary mt-4 w-1/2 uppercase"
+                    @click="emitPropostaAgendamento()">
+                    marcar {{ radioTipo }}
+                </button>
             </div>
-            <div class="w-1/4">
-                <multiselect
-                    v-model="selectInput"
-                    :options="selectOptions"
-                    class="custom-multiselect"
-                    :searchable="true"
-                    placeholder="Select an option"
-                    selected-label=""
-                    select-label=""
-                    deselect-label=""></multiselect>
-            </div>
-            <button class="btn btn-sm btn-primary mt-4 w-1/4 capitalize">
-                {{ radioTipo }}
-            </button>
         </div>
     </Transition>
 </template>
