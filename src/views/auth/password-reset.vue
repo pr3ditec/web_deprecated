05c20e118fd9ffb1bde9fcefb7b9c66793b69844
Adriano K <adriano.kasp@gmail.com>
2023-12-06 08:06:33 -0300

    Refactor background image styling in password-reset.vue

--- src/views/auth/password-reset.vue
+++ src/views/auth/password-reset.vue
@@ -25,7 +25,7 @@ useMeta({ title: "Reset Password" });
                 class="h-full w-full object-cover" />
         </div>
         <div
-            class="relative flex min-h-screen items-center justify-center bg-[url(/assets/images/auth/map.png)] bg-cover bg-center bg-no-repeat px-6 py-10 dark:bg-[#060818] sm:px-16">
+            class="relative flex min-h-screen items-center justify-center bg-cover bg-center bg-no-repeat px-6 py-10 dark:bg-[#060818] sm:px-16">
             <div
                 class="relative flex w-full max-w-[1502px] flex-col justify-between overflow-hidden rounded-md bg-white/60 backdrop-blur-lg dark:bg-black/50 lg:min-h-[758px] lg:flex-row lg:gap-10 xl:gap-0">
                 <div
@@ -39,8 +39,6 @@ useMeta({ title: "Reset Password" });
 
                 <div
                     class="relative hidden w-full items-center justify-center linear-background p-5 lg:inline-flex lg:max-w-[835px] xl:-ms-28 ltr:xl:skew-x-[14deg] rtl:xl:skew-x-[-14deg]">
-                    <div
-                        class="absolute inset-y-0 w-8 from-primary/10 via-transparent to-transparent ltr:-right-10 ltr:bg-gradient-to-r rtl:-left-10 rtl:bg-gradient-to-l xl:w-16 ltr:xl:-right-20 rtl:xl:-left-20"></div>
                     <div class="ltr:xl:-skew-x-[14deg] rtl:xl:skew-x-[14deg]">
                         <LogoFull />
                     </div>
