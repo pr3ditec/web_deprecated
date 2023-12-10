00e10fd66406c8c0f45559fb52788620953d8db8
Rodrigo Muller <rodrigofcmullerinfantaria@gmail.com>
2023-12-10 00:41:27 -0300

    🌈 style: modo dark

--- src/views/scheduling/pre-scheduling-modal.vue
+++ src/views/scheduling/pre-scheduling-modal.vue
@@ -100,7 +100,7 @@ export default {
             class="absolute inset-0 flex items-center justify-center w-full h-full top-0 left-0 bg-dark bg-opacity-50"
             style="z-index: 2000 !important">
             <div
-                class="flex flex-col items-center bg-white shadow-lg w-1/3 mx-auto p-5 rounded-lg">
+                class="flex flex-col items-center bg-white shadow-lg w-1/3 mx-auto p-5 rounded-lg dark:bg-slate-900 rounded-lg shadow-md dark:text-white">
                 <div class="flex flex-row w-full justify-end">
                     <span
                         class="text-2xl font-bold text-dark opacity-30 uppercase cursor-pointer"
@@ -132,7 +132,7 @@ export default {
                         <span class="uppercase">{{ $t("return") }}</span>
                     </label>
                 </div>
-                <div class="w-full p-4">
+                <div class="w-full p-4 dark:text-white">
                     <multiselect
                         v-show="preAgendamentoShowSelect"
                         v-model="selectInput"
@@ -152,6 +152,7 @@ export default {
                         :searchable="true"
                         placeholder="Selecione um paciente"></multiselect>
                 </div>
+
                 <button
                     class="btn btn-sm btn-primary mt-4 w-1/2 uppercase"
                     @click="emitPropostaAgendamento()"
