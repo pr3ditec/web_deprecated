694913b8caebc314c457eb06befaa7e5355dc0a9
Adriano K <adriano.kasp@gmail.com>
2023-12-10 02:19:50 -0300

    Add LocalhostIndicator component

---
+++ src/components/layout/LocalhostIndicator.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+import Host from "@/api/Host";
+import { useMeta } from "@/composables/use-meta";
+
+useMeta({ title: "Localhost Indicator" });
+
+let host = new Host();
+let isLocalhost = ref(false);
+
+(async () => {
+    isLocalhost.value = await host.isLocalhost();
+    console.log("isLocalhost", isLocalhost.value);
+})();
+</script>
+
+<template>
+    <span
+        v-if="isLocalhost"
+        class="text-black dark:text-white font-extrabold font-mono text-center">
+        LOCALHOST
+    </span>
+</template>
+
+<style scoped></style>
