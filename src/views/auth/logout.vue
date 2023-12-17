088aa0c77e5ff4b2ae9d2cd5a004035bad8f06bf
Adriano K <adriano.kasp@gmail.com>
2023-12-17 20:51:16 -0300

    Refactor logout.vue component

--- src/views/auth/logout.vue
+++ src/views/auth/logout.vue
@@ -4,21 +4,16 @@ const store = useAppStore();
 import { useRouter } from "vue-router";
 const router = useRouter();
 
-import { getBaseURL } from "@/helpers/Host";
-
-import { onMounted, reactive, ref } from "vue";
+import { onMounted } from "vue";
 
 onMounted(async () => {
-    //redirect to login page
     await store.logout();
     router.push("/login");
 });
 </script>
 
 <template>
-    <div class="text-center">
-        {{ $t('logging-out') }}...
-    </div>
+    <div class="text-center">{{ $t("logging-out") }}...</div>
 </template>
 
 <style lang="scss" scoped></style>
