6bdea8ee6d2095011bf6cd0b05b909dcd31b559d
leotrierveiler <leotrierveiler65@hotmail.com>
2024-01-10 23:40:54 -0300

    📃 docs: dados médico

--- src/views/documentation/documentation-autenticate.vue
+++ src/views/documentation/documentation-autenticate.vue
@@ -479,6 +479,7 @@ export default {
                     { name: 'usuario_id', type: 'int' },
                     { name: 'nome', type: 'string' },
                     { name: 'medico_id', type: 'int' },
+                    { name: 'sexo', type: 'char' },
                     {
                         name: 'documento',
                         type: 'array',
