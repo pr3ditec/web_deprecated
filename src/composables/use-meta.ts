8a07ddf5916f6224342abb69c98b5d92c83760f8
Lucas <123523359+lsbrel@users.noreply.github.com>
2024-01-26 09:36:58 -0300

    tab tittle

--- src/composables/use-meta.ts
+++ src/composables/use-meta.ts
@@ -12,5 +12,5 @@ export const usePageTitle = (pageTitle: any) =>
     );
 
 export const useMeta = (data: any) => {
-    return useHead({ ...data, title: `${data.title} - SauvviTech` });
+    return useHead({ ...data, title: `${data.title} - LsTech` });
 };
