3b3f367b91c52b527f514f4b2a2ed9fe6a9d471b
Lucas <123523359+lsbrel@users.noreply.github.com>
2024-01-29 16:01:33 -0300

    criar usuario

---
+++ src/components/layout/Section.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+    title: String,
+});
+</script>
+<template>
+    <div class="flex flex-col gap-1 mt-5">
+        <h1 class="capitalize font-semibold text-lg">{{ $t(title) }}</h1>
+        <hr class="text-dark dark:text-white p-2 mt-1" />
+        <div class="pl-3 pr-3">
+            <slot></slot>
+        </div>
+    </div>
+</template>
--- src/views/client/ClientNew.vue
+++ src/views/client/ClientNew.vue
@@ -2,9 +2,9 @@
 import { ref } from "vue";
 import InputText from "@/components/form/InputText.vue";
 import FormSelect from "@/components/form/FormSelect.vue";
-import FormCheckbox from "@/components/form/FormCheckbox.vue";
 import ButtonForm from "@/components/form/ButtonForm.vue";
 import Divider from "@/components/layout/Divider.vue";
+import Section from "@/components/layout/Section.vue";
 
 /** OPTIONS */
 const formData = ref({
@@ -39,107 +39,118 @@ const sendData = async () => {
 /** FUNCOES */
 </script>
 <template>
-    <div class="flex flex-col w-3/4 mx-auto">
+    <div class="flex flex-col w-5/6 mx-auto">
         <!-- CLIENTE -->
-        <InputText
-            label="name"
-            :required="true"
-            @update-value="(value: any) => (formData.cliente.nome = value)" />
-        <FormSelect
-            route="status-cliente"
-            name="status"
-            :required="true"
-            field="status"
-            @update-value="
-                (value: any) => (formData.cliente.status_cliente_id = value)
-            " />
+        <Section title="client">
+            <InputText
+                label="name"
+                :required="true"
+                @update-value="
+                    (value: any) => (formData.cliente.nome = value)
+                " />
+            <FormSelect
+                route="status-cliente"
+                name="status"
+                :required="true"
+                field="status"
+                @update-value="
+                    (value: any) => (formData.cliente.status_cliente_id = value)
+                " />
+        </Section>
         <!-- CLIENTE -->
 
         <Divider />
 
         <!-- TELEFONE -->
-        <InputText
-            label="telephone"
-            :required="true"
-            @update-value="
-                (value: any) => (formData.telefone.numero = value)
-            " />
-        <FormSelect
-            route="tipo-telefone"
-            name="phoneType"
-            :required="true"
-            field="tipo"
-            @update-value="
-                (value: any) => (formData.telefone.tipo_telefone_id = value)
-            " />
+        <Section title="telephone">
+            <InputText
+                label="telephone"
+                :required="true"
+                @update-value="
+                    (value: any) => (formData.telefone.numero = value)
+                " />
+            <FormSelect
+                route="tipo-telefone"
+                name="phoneType"
+                :required="true"
+                field="tipo"
+                @update-value="
+                    (value: any) => (formData.telefone.tipo_telefone_id = value)
+                " />
+        </Section>
         <!-- TELEFONE -->
 
         <Divider />
 
         <!-- DOCUMENTO -->
-        <InputText
-            label="document"
-            :required="true"
-            @update-value="
-                (value: any) => (formData.documento.numero = value)
-            " />
-        <FormSelect
-            route="tipo-documento"
-            name="docType"
-            :required="true"
-            field="tipo"
-            @update-value="
-                (value: any) => (formData.documento.tipo_documento_id = value)
-            " />
+        <Section title="document">
+            <InputText
+                label="number"
+                :required="true"
+                @update-value="
+                    (value: any) => (formData.documento.numero = value)
+                " />
+            <FormSelect
+                route="tipo-documento"
+                name="docType"
+                :required="true"
+                field="tipo"
+                @update-value="
+                    (value: any) =>
+                        (formData.documento.tipo_documento_id = value)
+                " />
+        </Section>
         <!-- DOCUMENTO -->
 
         <Divider />
 
         <!-- ENDERECO -->
-        <div class="flex flex-row gap-1">
-            <InputText
-                label="name"
+        <Section title="address">
+            <div class="flex flex-row gap-1">
+                <InputText
+                    label="name"
+                    :required="true"
+                    @update-value="
+                        (value: any) => (formData.endereco.nome = value)
+                    " />
+                <InputText
+                    label="cep"
+                    :required="true"
+                    :mask="'#####-###'"
+                    @update-value="
+                        (value: any) => (formData.endereco.cep = value)
+                    " />
+            </div>
+            <div class="flex flex-row gap-1">
+                <InputText
+                    label="street"
+                    :required="true"
+                    @update-value="
+                        (value: any) => (formData.endereco.rua = value)
+                    " />
+                <InputText
+                    label="neighborhood"
+                    :required="true"
+                    @update-value="
+                        (value: any) => (formData.endereco.bairro = value)
+                    " />
+                <InputText
+                    label="number"
+                    :required="true"
+                    @update-value="
+                        (value: any) => (formData.endereco.numero = value)
+                    " />
+            </div>
+            <FormSelect
+                route="cidade"
+                name="city"
+                field="nome"
+                :selected="0"
                 :required="true"
                 @update-value="
-                    (value: any) => (formData.endereco.nome = value)
+                    (value: any) => (formData.endereco.cidade_id = value)
                 " />
-            <InputText
-                label="cep"
-                :required="true"
-                :mask="'#####-###'"
-                @update-value="
-                    (value: any) => (formData.endereco.cep = value)
-                " />
-        </div>
-        <div class="flex flex-row gap-1">
-            <InputText
-                label="street"
-                :required="true"
-                @update-value="
-                    (value: any) => (formData.endereco.rua = value)
-                " />
-            <InputText
-                label="neighborhood"
-                :required="true"
-                @update-value="
-                    (value: any) => (formData.endereco.bairro = value)
-                " />
-            <InputText
-                label="number"
-                :required="true"
-                @update-value="
-                    (value: any) => (formData.endereco.numero = value)
-                " />
-        </div>
-        <FormSelect
-            route="cidade"
-            name="city"
-            field="nome"
-            :selected="0"
-            :required="true"
-            @update-value="
-                (value: any) => (formData.endereco.cidade_id = value)
-            " />
+        </Section>
         <!-- ENDERECO -->
 
         <Divider />
